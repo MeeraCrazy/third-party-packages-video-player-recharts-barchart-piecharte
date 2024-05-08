908bcd763a567d2fb08decb7a78e27aac7c4ca99
meeraCrazy <meeracrazy007@gmail.com>
2024-05-08 10:08:15 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import VideoPlayer from './components/VideoPlayer'
+import ReCharts from './components/ReCharts'
+import PieCharts from './components/PieCharts'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <VideoPlayer />
+    <ReCharts />
+    <PieCharts />
+  </>
+)
+
+export default App
